2946bda4bee1e45e3a0769b74e76afda9fef7275
Gelo X <grzesie2000@gmail.com>
2024-05-05 18:14:33 +0200

    from beginning to challange1

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log("I can drive.");
+*/
+
+function logger() {
+    console.log("My name is Grzegorz!");
+}
+logger();
+logger();
+logger();
+//I stopped writing the code, from tutorial cause it's obvious to me at this point.
